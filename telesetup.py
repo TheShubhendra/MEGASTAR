48c9610b923f5cf678e19e19c239017758952d2e
Roy <71298965+suhaash02@users.noreply.github.com>
2020-11-08 08:33:11 +0530

    Update telesetup.py

--- telesetup.py
+++ telesetup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # (c) https://t.me/TelethonChat/37677
-# This Source Code Form is subject to the terms of the GNU
+# This Source Code Form is subject to the terms of the GNU hatsoff
 # General Public License, v.3.0. If a copy of the GPL was not distributed with this
 # file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.
 
