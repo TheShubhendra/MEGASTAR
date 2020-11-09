b03b57df337ec0e0be024c46897238ae1f3180bc
Roy <71298965+suhaash02@users.noreply.github.com>
2020-11-09 13:31:49 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -42,10 +42,10 @@ for name in files:
         if shortname.replace(".py", "") not in Config.NO_LOAD:
             load_module(shortname.replace(".py", ""))
 
-LOGS.info("Yay your userbot is officially working.!!!")
+LOGS.info("kek!! your viper is poisonous now.!!!")
 LOGS.info(
-    "Congratulation, now type .alive to see message if bot is live\n"
-    "If you need assistance, head to https://t.me/catuserbot_support"
+    "Congratulations, now type .alive to view viper is redy to hit\n"
+    "support and thank @anonymous for this awsm bot"
 )
 
 if len(argv) not in (1, 3, 4):
