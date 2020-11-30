c25edc63de04629746b7dfe9ff456c14dd474d10
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2020-11-30 12:38:46 +0530

    Update and rename inline to secret.py

--- userbot/plugins/secret.py
+++ userbot/plugins/secret.py
@@ -1,5 +1,3 @@
-# thanks to @null7410  for callbackquery code
-# created by @sandy1709 and @mrconfused
 import re
 
 from telethon import custom, events
@@ -25,15 +23,15 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 try:
                     u = await event.client.get_entity(u)
                     if u.username:
-                        sandy = f"@{u.username}"
+                        aritra = f"@{u.username}"
                     else:
-                        sandy = f"[{u.first_name}](tg://user?id={u.id})"
+                        aritra = f"[{u.first_name}](tg://user?id={u.id})"
                 except ValueError:
                     # ValueError: Could not find the input entity
-                    sandy = f"[user](tg://user?id={u})"
+                    aritra = f"[user](tg://user?id={u})"
                 result = builder.article(
                     title="secret message",
-                    text=f"🔒 A whisper message to {sandy}, Only he/she can open it.",
+                    text=f"🔒 A whisper message to {aritra}, Only he/she can open it.",
                     buttons=buttons,
                 )
                 await event.answer([result] if result else None)
@@ -46,12 +44,12 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     )
                 ]
                 if u.username:
-                    sandy = f"@{u.username}"
+                    aritra = f"@{u.username}"
                 else:
-                    sandy = f"[{u.first_name}](tg://user?id={u.id})"
+                    aritra = f"[{u.first_name}](tg://user?id={u.id})"
                 result = builder.article(
                     title="secret message",
-                    text=f"🔒 A whisper message to {sandy}, Only he/she can open it.",
+                    text=f"🔒 A whisper message to {aritra}, Only he/she can open it.",
                     buttons=buttons,
                 )
                 await event.answer([result] if result else None)
