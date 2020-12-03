5f1d3275ad943f3b2c53da930079ec1e3b37d3dd
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2020-12-03 11:03:09 +0530

    Update heroku_config.py

--- heroku_config.py
+++ heroku_config.py
@@ -3,7 +3,7 @@ import os
 
 class var((object)):
     APP_ID = int(os.environ.get("APP_ID", 7))
-    # 6 is a placeholder
+    # 7 is a placeholder
     API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     DB_URI = os.environ.get("DATABASE_URL", None)
