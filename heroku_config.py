155bcfe9dc8a2062ee57857033ff13ee094a2219
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2020-12-03 11:03:52 +0530

    Update heroku_config.py

--- heroku_config.py
+++ heroku_config.py
@@ -1,7 +1,7 @@
 import os
 
 
-class var((object)):
+class Var(object):
     APP_ID = int(os.environ.get("APP_ID", 7))
     # 7 is a placeholder
     API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
