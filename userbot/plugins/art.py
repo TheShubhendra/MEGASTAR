a69ab0f9dea6f8bb296bdf8fe7d61a2116c47097
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2020-12-05 16:28:41 +0530

    Update art.py

--- userbot/plugins/art.py
+++ userbot/plugins/art.py
@@ -6,7 +6,6 @@ DEF = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 
 
 @bot.on(admin_cmd(pattern="ded (.*)"))
-@bot.on(sudo_cmd(pattern="ded (.*)", allow_sudo=True))
 async def kakashi(ded):
     name = ded.pattern_match.group(1)
     await edit_or_reply(
@@ -29,7 +28,6 @@ async def kakashi(ded):
 
 
 @bot.on(admin_cmd(pattern="killer (.*)"))
-@bot.on(sudo_cmd(pattern="killer (.*)", allow_sudo=True))
 async def kakashi(killer):
     name = killer.pattern_match.group(1)
     await edit_or_reply(
@@ -320,84 +318,70 @@ R = (
 
 
 @bot.on(admin_cmd(pattern=r"monster$"))
-@bot.on(sudo_cmd(pattern="monster$", allow_sudo=True))
 async def bluedevilmonster(monster):
     await edit_or_reply(monster, A)
 
 
 @bot.on(admin_cmd(pattern=r"pig$"))
-@bot.on(sudo_cmd(pattern="pig$", allow_sudo=True))
 async def bluedevilpig(pig):
     await edit_or_reply(pig, B)
 
 
 @bot.on(admin_cmd(pattern=r"gun$"))
-@bot.on(sudo_cmd(pattern="gun$", allow_sudo=True))
 async def bluedevilgun(gun):
     await edit_or_reply(gun, D)
 
 
 @bot.on(admin_cmd(pattern=r"dog$"))
-@bot.on(sudo_cmd(pattern="dog$", allow_sudo=True))
 async def bluedevildog(dog):
     await edit_or_reply(dog, E)
 
 
 @bot.on(admin_cmd(pattern=r"hello$"))
-@bot.on(sudo_cmd(pattern="hello$", allow_sudo=True))
 async def bluedevilhello(hello):
     await edit_or_reply(hello, F)
 
 
 @bot.on(admin_cmd(pattern=r"hmf$"))
-@bot.on(sudo_cmd(pattern="hmf$", allow_sudo=True))
 async def bluedevilhmf(hmf):
     await edit_or_reply(hmf, G)
 
 
 @bot.on(admin_cmd(pattern=r"couple$"))
-@bot.on(sudo_cmd(pattern="couple$", allow_sudo=True))
 async def bluedevilcouple(couple):
     await edit_or_reply(couple, H)
 
 
 @bot.on(admin_cmd(pattern=r"sup$"))
-@bot.on(sudo_cmd(pattern="sup$", allow_sudo=True))
 async def bluedevilsupreme(supreme):
     await edit_or_reply(supreme, I)
 
 
 @bot.on(admin_cmd(pattern=r"india$"))
-@bot.on(sudo_cmd(pattern="india$", allow_sudo=True))
 async def bluedevilindia(india):
     await edit_or_reply(india, J)
 
 
 @bot.on(admin_cmd(pattern=r"wc$"))
-@bot.on(sudo_cmd(pattern="wc$", allow_sudo=True))
 async def bluedevilwelcome(welcome):
     await edit_or_reply(welcome, K)
 
 
 @bot.on(admin_cmd(pattern=r"snk$"))
-@bot.on(sudo_cmd(pattern="snk$", allow_sudo=True))
 async def bluedevilsnake(snake):
     await edit_or_reply(snake, L)
 
 
 @bot.on(admin_cmd(pattern=r"bye$"))
-@bot.on(sudo_cmd(pattern="bye$", allow_sudo=True))
 async def bluedevilbye(bye):
     await edit_or_reply(bye, O)
 
 
 @bot.on(admin_cmd(pattern=r"shitos$"))
-@bot.on(sudo_cmd(pattern="shitos$", allow_sudo=True))
 async def bluedevilshitos(shitos):
     await edit_or_reply(shitos, P)
 
 
 @bot.on(admin_cmd(pattern=r"dislike$"))
-@bot.on(sudo_cmd(pattern="dislike$", allow_sudo=True))
 async def bluedevildislike(dislike):
     await edit_or_reply(dislike, R)
