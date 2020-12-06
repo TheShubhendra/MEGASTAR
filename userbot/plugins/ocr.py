391afdbaed98b35a26f8dd3ff9ab4781d638cdad
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2020-12-06 23:51:32 +0530

    Update ocr.py

--- userbot/plugins/ocr.py
+++ userbot/plugins/ocr.py
@@ -8,7 +8,7 @@ import os
 
 import requests
 
-from ..utils import admin_cmd, edit_or_reply, sudo_cmd
+from ..utils import admin_cmd, edit_or_reply
 from . import CMD_HELP
 
 OCR_SPACE_API_KEY = Config.OCR_SPACE_API_KEY
@@ -45,9 +45,8 @@ async def ocr_space_file(
 
 
 @bot.on(admin_cmd(pattern="ocr(?: |$)(.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="ocr(?: |$)(.*)", allow_sudo=True))
 async def ocr(event):
-    catevent = await edit_or_reply(event, "`Reading...`")
+    event = await edit_or_reply(event, "`Reading...`")
     if not os.path.isdir(Config.TEMP_DIR):
         os.makedirs(Config.TEMP_DIR)
     lang_code = event.pattern_match.group(1)
@@ -58,9 +57,9 @@ async def ocr(event):
     try:
         ParsedText = test_file["ParsedResults"][0]["ParsedText"]
     except BaseException:
-        await catevent.edit("`Couldn't read it.`\n`I guess I need new glasses.`")
+        await event.edit("`Couldn't read it.`\n`I guess I need new glasses.`")
     else:
-        await catevent.edit(f"`Here's what I could read from it:`\n\n{ParsedText}")
+        await event.edit(f"`Here's what I could read from it:`\n\n{ParsedText}")
     os.remove(downloaded_file_name)
 
 
