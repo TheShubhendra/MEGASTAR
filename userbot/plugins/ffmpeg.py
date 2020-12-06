74ad59736e541213f934a1e6704900d192293eb9
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2020-12-06 08:52:25 +0530

    Update ffmpeg.py

--- userbot/plugins/ffmpeg.py
+++ userbot/plugins/ffmpeg.py
@@ -5,13 +5,12 @@ import os
 import time
 from datetime import datetime
 
-from ..utils import admin_cmd, edit_or_reply, progress, sudo_cmd
+from ..utils import admin_cmd, edit_or_reply, progress, 
 
 FF_MPEG_DOWN_LOAD_MEDIA_PATH = "unievent.client.media.ffmpeg"
 
 
 @bot.on(admin_cmd(pattern="ffmpegsave$"))
-@bot.on(sudo_cmd(pattern="ffmpegsave$", allow_sudo=True))
 async def ff_mpeg_trim_cmd(event):
     if event.fwd_from:
         return
@@ -51,7 +50,6 @@ async def ff_mpeg_trim_cmd(event):
 
 
 @bot.on(admin_cmd(pattern="ffmpegtrim"))
-@bot.on(sudo_cmd(pattern="ffmpegtrim", allow_sudo=True))
 async def ff_mpeg_trim_cmd(event):
     if event.fwd_from:
         return
