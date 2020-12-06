5bc931e3aa0b4fc4ce5b5c3c970393df2b702122
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2020-12-06 23:34:53 +0530

    Update quotly.py

--- userbot/plugins/quotly.py
+++ userbot/plugins/quotly.py
@@ -9,44 +9,42 @@ from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 from .. import CMD_HELP, process
-from ..utils import admin_cmd, edit_or_reply, sudo_cmd
+from ..utils import admin_cmd, edit_or_reply
 
 
 @bot.on(admin_cmd(pattern="q(?: |$)(.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="q(?: |$)(.*)", allow_sudo=True))
-async def stickerchat(catquotes):
-    if catquotes.fwd_from:
+async def stickerchat(quotes):
+    if quotes.fwd_from:
         return
-    reply = await catquotes.get_reply_message()
+    reply = await quotes.get_reply_message()
     if not reply:
         await edit_or_reply(
-            catquotes, "`I cant quote the message . reply to a message`"
+            quotes, "`I cant quote the message . reply to a message`"
         )
         return
     fetchmsg = reply.message
     repliedreply = await reply.get_reply_message()
     if reply.media and reply.media.document.mime_type in ("mp4"):
-        await edit_or_reply(catquotes, "`this format is not supported now`")
+        await edit_or_reply(quotes, "`this format is not supported now`")
         return
-    catevent = await edit_or_reply(catquotes, "`Making quote...`")
+    event = await edit_or_reply(quotes, "`Making quote...`")
     user = (
         await event.client.get_entity(reply.forward.sender)
         if reply.fwd_from
         else reply.sender
     )
-    res, catmsg = await process(fetchmsg, user, catquotes.client, reply, repliedreply)
+    res, msg = await process(fetchmsg, user, quotes.client, reply, repliedreply)
     if not res:
         return
-    catmsg.save("./temp/sticker.webp")
-    await catquotes.client.send_file(
-        catquotes.chat_id, "./temp/sticker.webp", reply_to=reply
+    msg.save("./temp/sticker.webp")
+    await quotes.client.send_file(
+        quotes.chat_id, "./temp/sticker.webp", reply_to=reply
     )
-    await catevent.delete()
+    await event.delete()
     os.remove("./temp/sticker.webp")
 
 
 @bot.on(admin_cmd(pattern="qbot(?: |$)(.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="qbot(?: |$)(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -58,7 +56,7 @@ async def _(event):
         await edit_or_reply(event, "```Reply to text message```")
         return
     chat = "@QuotLyBot"
-    catevent = await edit_or_reply(event, "```Making a Quote```")
+    event = await edit_or_reply(event, "```Making a Quote```")
     async with event.client.conversation(chat) as conv:
         try:
             response = conv.wait_event(
@@ -67,15 +65,15 @@ async def _(event):
             await event.client.forward_messages(chat, reply_message)
             response = await response
         except YouBlockedUserError:
-            await catevent.edit("```Please unblock me (@QuotLyBot) u Nigga```")
+            await event.edit("```Please unblock me (@QuotLyBot) u Nigga```")
             return
         await event.client.send_read_acknowledge(conv.chat_id)
         if response.text.startswith("Hi!"):
-            await catevent.edit(
+            await event.edit(
                 "```Can you kindly disable your forward privacy settings for good?```"
             )
         else:
-            await catevent.delete()
+            await event.delete()
             await event.client.send_message(event.chat_id, response.message)
 
 
