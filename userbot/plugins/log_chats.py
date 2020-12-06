58d6c97b9f35585b8d4618e73c008ae5cc81349b
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2020-12-06 14:03:38 +0530

    Update log_chats.py

--- userbot/plugins/log_chats.py
+++ userbot/plugins/log_chats.py
@@ -7,7 +7,7 @@ import userbot.plugins.sql_helper.no_log_pms_sql as no_log_pms_sql
 
 from ..utils import admin_cmd
 from . import BOTLOG, BOTLOG_CHATID, CMD_HELP, LOGS, mentionuser
-
+  
 RECENT_USER = None
 NEWPM = None
 COUNT = 0
