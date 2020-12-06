3da76ae7af0bb666bf8d17c8513419a4d8cdcabb
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2020-12-06 22:15:08 +0530

    Update schedule.py

--- userbot/plugins/schedule.py
+++ userbot/plugins/schedule.py
@@ -1,15 +1,14 @@
 from asyncio import sleep
 
 from .. import CMD_HELP
-from ..utils import admin_cmd, sudo_cmd
+from ..utils import admin_cmd
 
 
 @bot.on(admin_cmd(pattern="schd (\d*) (.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="schd (\d*) (.*)", allow_sudo=True))
 async def _(event):
-    cat = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
-    message = cat[1]
-    ttl = int(cat[0])
+    lub = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
+    message = lub[1]
+    ttl = int(lub[0])
     try:
         await event.delete()
     except:
