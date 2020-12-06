f38437635bdb0c73265c317ecd09ddab3e9d6d93
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2020-12-06 10:59:57 +0530

    Update gps.py

--- userbot/plugins/gps.py
+++ userbot/plugins/gps.py
@@ -17,7 +17,7 @@ from geopy.geocoders import Nominatim
 from telethon.tl import types
 
 from userbot import CMD_HELP
-from userbot.utils import admin_cmd, sudo_cmd
+from userbot.utils import admin_cmd
 
 
 @bot.on(admin_cmd(pattern="gps ?(.*)"))
@@ -34,7 +34,7 @@ async def gps(event):
 
     await event.edit("finding")
 
-    geolocator = Nominatim(user_agent="catuserbot")
+    geolocator = Nominatim(user_agent="legenduserbot")
     geoloc = geolocator.geocode(input_str)
 
     if geoloc:
@@ -47,33 +47,6 @@ async def gps(event):
     else:
         await event.edit("i coudn't find it")
 
-
-@bot.on(sudo_cmd(pattern="gps ?(.*)", allow_sudo=True))
-async def gps(event):
-    if event.fwd_from:
-        return
-    reply_to_id = event.message
-    if event.reply_to_msg_id:
-        reply_to_id = await event.get_reply_message()
-    input_str = event.pattern_match.group(1)
-
-    if not input_str:
-        return await event.reply("what should I find give me location.")
-
-    cat = await event.reply("finding")
-
-    geolocator = Nominatim(user_agent="catuserbot")
-    geoloc = geolocator.geocode(input_str)
-
-    if geoloc:
-        lon = geoloc.longitude
-        lat = geoloc.latitude
-        await reply_to_id.reply(
-            input_str, file=types.InputMediaGeoPoint(types.InputGeoPoint(lat, lon))
-        )
-        await cat.delete()
-    else:
-        await cat.edit("i coudn't find it")
 
 
 CMD_HELP.update(
