1a46648769285d63b60b72ccb116e2d244c45b98
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2020-12-06 12:34:25 +0530

    Update imposter.py

--- userbot/plugins/imposter.py
+++ userbot/plugins/imposter.py
@@ -1,20 +1,17 @@
 """
-Created by @Jisan7509
-Credit @Infinity20998
-Userbot plugin fot CatUserbot
+Userbot plugin fot legend ub
 """
 
 
 import asyncio
 
-from ..utils import admin_cmd, edit_or_reply, sudo_cmd
+from ..utils import admin_cmd, edit_or_reply
 from . import ALIVE_NAME, CMD_HELP
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "legend"
 
 
 @bot.on(admin_cmd(pattern="imp(|n) (.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="imp(|n) (.*)", allow_sudo=True))
 async def _(event):
     hmm = bot.uid
     USERNAME = f"tg://user?id={hmm}"
@@ -24,7 +21,7 @@ async def _(event):
     await asyncio.sleep(2)
     await text1.delete()
     stcr1 = await event.client.send_file(
-        event.chat_id, "CAADAQADRwADnjOcH98isYD5RJTwAg"
+        event.chat_id, "Tobw7BkaeSFbwMqbkIhKiw"
     )
     text2 = await event.reply(
         f"**[{DEFAULTUSER}]({USERNAME}) :** I have to call discussion"
@@ -33,7 +30,7 @@ async def _(event):
     await stcr1.delete()
     await text2.delete()
     stcr2 = await event.client.send_file(
-        event.chat_id, "CAADAQADRgADnjOcH9odHIXtfgmvAg"
+        event.chat_id, "Tobw7BkaeSFbwMqbkIhKiw"
     )
     text3 = await event.reply(
         f"**[{DEFAULTUSER}]({USERNAME}) :** We have to eject the imposter or will lose "
@@ -42,7 +39,7 @@ async def _(event):
     await stcr2.delete()
     await text3.delete()
     stcr3 = await event.client.send_file(
-        event.chat_id, "CAADAQADOwADnjOcH77v3Ap51R7gAg"
+        event.chat_id, "Tobw7BkaeSFbwMqbkIhKiw"
     )
     text4 = await event.reply(f"**Others :** Where??? ")
     await asyncio.sleep(2)
@@ -59,78 +56,77 @@ async def _(event):
     stcr4 = await event.client.send_file(
         event.chat_id, "CAADAQADLwADnjOcH-wxu-ehy6NRAg"
     )
-    catevent = await event.reply(f"{name} is ejected.......")
+    legendevent = await event.reply(f"{name} is ejected.......")
     await asyncio.sleep(2)
-    await catevent.edit("ඞㅤㅤㅤㅤ ㅤㅤㅤㅤ")
+    await legendevent.edit("ඞㅤㅤㅤㅤ ㅤㅤㅤㅤ")
     await asyncio.sleep(0.5)
-    await catevent.edit("ㅤඞㅤㅤㅤㅤ ㅤㅤㅤ")
+    await legendevent.edit("ㅤඞㅤㅤㅤㅤ ㅤㅤㅤ")
     await asyncio.sleep(0.5)
-    await catevent.edit("ㅤㅤ ඞㅤㅤㅤㅤㅤㅤ")
+    await legendevent.edit("ㅤㅤ ඞㅤㅤㅤㅤㅤㅤ")
     await asyncio.sleep(0.5)
-    await catevent.edit("ㅤㅤㅤ ඞㅤㅤㅤㅤㅤ")
+    await legendevent.edit("ㅤㅤㅤ ඞㅤㅤㅤㅤㅤ")
     await asyncio.sleep(0.5)
-    await catevent.edit("ㅤㅤㅤㅤ ඞㅤㅤㅤㅤ")
+    await legendevent.edit("ㅤㅤㅤㅤ ඞㅤㅤㅤㅤ")
     await asyncio.sleep(0.5)
-    await catevent.edit("ㅤㅤㅤㅤㅤ ඞㅤㅤㅤ")
+    await legendevent.edit("ㅤㅤㅤㅤㅤ ඞㅤㅤㅤ")
     await asyncio.sleep(0.5)
-    await catevent.edit("ㅤㅤㅤㅤㅤㅤ ඞㅤㅤ")
+    await legendevent.edit("ㅤㅤㅤㅤㅤㅤ ඞㅤㅤ")
     await asyncio.sleep(0.5)
-    await catevent.edit("ㅤㅤㅤㅤㅤㅤㅤ ඞㅤ")
+    await legendevent.edit("ㅤㅤㅤㅤㅤㅤㅤ ඞㅤ")
     await asyncio.sleep(0.5)
-    await catevent.edit("ㅤㅤㅤㅤㅤㅤㅤㅤ ඞ")
+    await legendevent.edit("ㅤㅤㅤㅤㅤㅤㅤㅤ ඞ")
     await asyncio.sleep(0.5)
-    await catevent.edit("ㅤㅤㅤㅤㅤㅤㅤㅤ ㅤ")
+    await legendevent.edit("ㅤㅤㅤㅤㅤㅤㅤㅤ ㅤ")
     await asyncio.sleep(0.2)
     await stcr4.delete()
     if cmd == "":
-        await catevent.edit(
+        await legendevent.edit(
             f". 　　　。　　　　•　 　ﾟ　　。 　　.\n .　　　 　　.　　　　　。　　 。　. 　\n\n  . 　　 。   　     ඞ         。 . 　　 • 　　　　•\n\n  ﾟ{name} was an Imposter.      。　. 　 　       。　.                                        。　. \n                                   　.          。　  　. \n　'         0 Impostor remains    　 。　.  　　.                。　.        。 　     .          。 　            .               .         .    ,      。\n　　ﾟ　　　.　　.    ,　 　。　 　. 　 .     。"
         )
         await asyncio.sleep(4)
-        await catevent.delete()
+        await legendevent.delete()
         await event.client.send_file(event.chat_id, "CAADAQADLQADnjOcH39IqwyR6Q_0Ag")
     elif cmd == "n":
-        await catevent.edit(
+        await legendevent.edit(
             f". 　　　。　　　　•　 　ﾟ　　。 　　.\n .　　　 　　.　　　　　。　　 。　. 　\n\n  . 　　 。   　     ඞ         。 . 　　 • 　　　　•\n\n  ﾟ{name} was not an Imposter.      。　. 　 　       。　.                                        。　. \n                                   　.          。　  　. \n　'         1 Impostor remains    　 。　.  　　.                。　.        。 　     .          。 　            .               .         .    ,      。\n　　ﾟ　　　.　　.    ,　 　。　 　. 　 .     。"
         )
         await asyncio.sleep(4)
-        await catevent.delete()
+        await legendevent.delete()
         await event.client.send_file(event.chat_id, "CAADAQADQAADnjOcH-WOkB8DEctJAg")
 
 
 @bot.on(admin_cmd(pattern="timp(|n) (.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="timp(|n) (.*)", allow_sudo=True))
 async def _(event):
     name = event.pattern_match.group(2)
     cmd = event.pattern_match.group(1).lower()
-    catevent = await edit_or_reply(event, f"{name} is ejected.......")
+    legendevent = await edit_or_reply(event, f"{name} is ejected.......")
     await asyncio.sleep(2)
-    await catevent.edit("ඞㅤㅤㅤㅤ ㅤㅤㅤㅤ")
+    await legendevent.edit("ඞㅤㅤㅤㅤ ㅤㅤㅤㅤ")
     await asyncio.sleep(0.8)
-    await catevent.edit("ㅤඞㅤㅤㅤㅤ ㅤㅤㅤ")
+    await legendevent.edit("ㅤඞㅤㅤㅤㅤ ㅤㅤㅤ")
     await asyncio.sleep(0.8)
-    await catevent.edit("ㅤㅤ ඞㅤㅤㅤㅤㅤㅤ")
+    await legendevent.edit("ㅤㅤ ඞㅤㅤㅤㅤㅤㅤ")
     await asyncio.sleep(0.8)
-    await catevent.edit("ㅤㅤㅤ ඞㅤㅤㅤㅤㅤ")
+    await legendevent.edit("ㅤㅤㅤ ඞㅤㅤㅤㅤㅤ")
     await asyncio.sleep(0.8)
-    await catevent.edit("ㅤㅤㅤㅤ ඞㅤㅤㅤㅤ")
+    await legendevent.edit("ㅤㅤㅤㅤ ඞㅤㅤㅤㅤ")
     await asyncio.sleep(0.8)
-    await catevent.edit("ㅤㅤㅤㅤㅤ ඞㅤㅤㅤ")
+    await legendevent.edit("ㅤㅤㅤㅤㅤ ඞㅤㅤㅤ")
     await asyncio.sleep(0.8)
-    await catevent.edit("ㅤㅤㅤㅤㅤㅤ ඞㅤㅤ")
+    await legendevent.edit("ㅤㅤㅤㅤㅤㅤ ඞㅤㅤ")
     await asyncio.sleep(0.8)
-    await catevent.edit("ㅤㅤㅤㅤㅤㅤㅤ ඞㅤ")
+    await legendevent.edit("ㅤㅤㅤㅤㅤㅤㅤ ඞㅤ")
     await asyncio.sleep(0.8)
-    await catevent.edit("ㅤㅤㅤㅤㅤㅤㅤㅤ ඞ")
+    await legendevent.edit("ㅤㅤㅤㅤㅤㅤㅤㅤ ඞ")
     await asyncio.sleep(0.8)
-    await catevent.edit("ㅤㅤㅤㅤㅤㅤㅤㅤ ㅤ")
+    await legendevent.edit("ㅤㅤㅤㅤㅤㅤㅤㅤ ㅤ")
     await asyncio.sleep(0.2)
     if cmd == "":
-        await catevent.edit(
+        await legendevent.edit(
             f". 　　　。　　　　•　 　ﾟ　　。 　　.\n .　　　 　　.　　　　　。　　 。　. 　\n\n  . 　　 。   　     ඞ         。 . 　　 • 　　　　•\n\n  ﾟ {name} was an Imposter.      。　. 　 　       。　.                                        。　. \n                                   　.          。　  　. \n　'         0 Impostor remains    　 。　.  　　.                。　.        。 　     .          。 　            .               .         .    ,      。\n　　ﾟ　　　.　　.    ,　 　。　 　. 　 .     。"
         )
     elif cmd == "n":
-        await catevent.edit(
+        await legendevent.edit(
             f". 　　　。　　　　•　 　ﾟ　　。 　　.\n .　　　 　　.　　　　　。　　 。　. 　\n\n  . 　　 。   　     ඞ         。 . 　　 • 　　　　•\n\n  ﾟ {name} was not an Imposter.      。　. 　 　       。　.                                        。　. \n                                   　.          。　  　. \n　'         1 Impostor remains    　 。　.  　　.                。　.        。 　     .          。 　            .               .         .    ,      。\n　　ﾟ　　　.　　.    ,　 　。　 　. 　 .     。"
         )
 
