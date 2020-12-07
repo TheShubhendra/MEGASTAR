fc30e2a0eb95f24522acc37c66236b1500dc0400
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2020-12-07 10:41:01 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -6,7 +6,6 @@ from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
 import os
 from telethon import TelegramClient
 from var import Var
-from userbot.utils import load_module
 from userbot import LOAD_PLUG, BOTLOG_CHATID, LOGS
 from pathlib import Path
 import asyncio
