61d3556b41274e8fbf4840ab069148f0ee851bcb
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2020-12-10 20:28:29 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -11,9 +11,6 @@ import re
 import logging
 import inspect
 
-handler = Config.CMD_HNDLR if Config.CMD_HNDLR else r"\."
-sudo_hndlr = Config.SUDO_HNDLR if Config.SUDO_HNDLR else "!"
-
 
 def command(**args):
     args["func"] = lambda e: e.via_bot_id is None
