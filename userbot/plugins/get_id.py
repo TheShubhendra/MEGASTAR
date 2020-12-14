54f45968af8a3a4266345f9f94150aff123c19d8
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2020-12-14 23:54:06 +0530

    Update get_id.py

--- userbot/plugins/get_id.py
+++ userbot/plugins/get_id.py
@@ -4,7 +4,7 @@ from ..utils import admin_cmd, edit_or_reply
 from . import CMD_HELP
 
 
-@bot.on(admin_cmd(pattern="(get_id|id)( (.*)|$)"))
+@borg.on(admin_cmd(pattern="(get_id|id)( (.*)|$)"))
 async def _(event):
     if event.fwd_from:
         return
