1f2fb0fe99313510c87efc5f80f15519fe3c6834
Krishan <73457860+krishan442@users.noreply.github.com>
2020-12-19 20:45:14 +0530

    Update mega.py

--- userbot/plugins/mega.py
+++ userbot/plugins/mega.py
@@ -53,7 +53,7 @@ async def subprocess_run(megadl, cmd):
     return stdout.decode().strip(), stderr.decode().strip(), exitCode
 
 
-@bot.on(admin_cmd(outgoing=True, pattern=r"mega(?: |$)(.*)"))
+@borg.on(admin_cmd(outgoing=True, pattern=r"mega(?: |$)(.*)"))
 async def mega_downloader(megadl):
     catevent = await edit_or_reply(megadl, "`Collecting information...`")
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
