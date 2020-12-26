9bbd41083dd0fec0b8cd77468d9b875aa25d5e4d
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2020-12-26 17:45:37 +0530

    Update collage.py

--- userbot/plugins/collage.py
+++ userbot/plugins/collage.py
@@ -6,66 +6,66 @@
 import os
 
 from ..utils import admin_cmd, edit_or_reply
-from . import CMD_HELP, make_gif, runcmd
+from . import CMD_HELP
 
 
 @borg.on(admin_cmd(pattern="collage(?: |$)(.*)", outgoing=True))
-async def collage(cat):
-    catinput = cat.pattern_match.group(1)
-    reply = await cat.get_reply_message()
-    catid = cat.reply_to_msg_id
-    cat = await edit_or_reply(
-        cat, "```collaging this may take several minutes too..... 😁```"
+async def collage(pgl):
+    pglinput = pgl.pattern_match.group(1)
+    reply = await pgl.get_reply_message()
+    pglid = pgl.reply_to_msg_id
+    pgl = await edit_or_reply(
+        pgl, "```collaging this may take several minutes too..... 😁```"
     )
     if not (reply and (reply.media)):
-        await cat.edit("`Media not found...`")
+        await pgl.edit("`Media not found...`")
         return
     if not os.path.isdir("./temp/"):
         os.mkdir("./temp/")
-    catsticker = await reply.download_media(file="./temp/")
-    if not catsticker.endswith((".mp4", ".mkv", ".tgs")):
-        os.remove(catsticker)
-        await cat.edit("`Media format is not supported...`")
+    pglsticker = await reply.download_media(file="./temp/")
+    if not pglsticker.endswith((".mp4", ".mkv", ".tgs")):
+        os.remove(pglsticker)
+        await pgl.edit("`Media format is not supported...`")
         return
-    if catinput:
-        if not catinput.isdigit():
-            os.remove(catsticker)
-            await cat.edit("`You input is invalid, check help`")
+    if pglinput:
+        if not pglinput.isdigit():
+            os.remove(pglsticker)
+            await pgl.edit("`You input is invalid, check help`")
             return
-        catinput = int(catinput)
-        if not 0 < catinput < 10:
-            os.remove(catsticker)
-            await cat.edit(
+        pglinput = int(pglinput)
+        if not 0 < pglinput < 10:
+            os.remove(pglsticker)
+            await pgl.edit(
                 "`Why too big grid you cant see images, use size of grid between 1 to 9`"
             )
             return
     else:
-        catinput = 3
-    if catsticker.endswith(".tgs"):
-        hmm = await make_gif(cat, catsticker)
+        pglinput = 3
+    if pglsticker.endswith(".tgs"):
+        hmm = await make_gif(pgl, pglsticker)
         if hmm.endswith(("@tgstogifbot")):
-            os.remove(catsticker)
-            return await cat.edit(hmm)
+            os.remove(pglsticker)
+            return await pgl.edit(hmm)
         collagefile = hmm
     else:
-        collagefile = catsticker
+        collagefile = pglsticker
     endfile = "./temp/collage.png"
-    catcmd = f"vcsi -g {catinput}x{catinput} {collagefile} -o {endfile}"
-    stdout, stderr = (await runcmd(catcmd))[:2]
+    pglcmd = f"vcsi -g {pglinput}x{pglinput} {collagefile} -o {endfile}"
+    stdout, stderr = (await runcmd(pglcmd))[:2]
     if not os.path.exists(endfile):
-        for files in (catsticker, collagefile):
+        for files in (pglsticker, collagefile):
             if files and os.path.exists(files):
                 os.remove(files)
         return await edit_delete(
-            cat, f"`media is not supported or try with smaller grid size`", 5
+            pgl, f"`media is not supported or try with smaller grid size`", 5
         )
-    await cat.client.send_file(
-        cat.chat_id,
+    await pgl.client.send_file(
+        pgl.chat_id,
         endfile,
-        reply_to=catid,
+        reply_to=pglid,
     )
-    await cat.delete()
-    for files in (catsticker, collagefile, endfile):
+    await pgl.delete()
+    for files in (pglsticker, collagefile, endfile):
         if files and os.path.exists(files):
             os.remove(files)
 
