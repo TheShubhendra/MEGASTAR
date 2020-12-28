9dfe3144359893c9203001902e2c71c27c2265a6
Bristi-OP <missbristimandal@gmail.com>
2020-12-28 03:47:31 +0000

    Pylint Fixes 🚶

--- userbot/plugins/song.py
+++ userbot/plugins/song.py
@@ -1,24 +1,18 @@
-#no credit line.. bcoz plugin died
+# no credit line.. bcoz plugin died
 
-from telethon import events
-import subprocess
-from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
-import io
-import asyncio
-import time
-from userbot.utils import admin_cmd
 import glob
-import os
-import spotdl
 import subprocess
+
+from userbot.utils import admin_cmd
+
 
 @borg.on(admin_cmd(pattern="getsong ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
-    await event.edit("**Getting Your Music it may take a few seconds to fetch the song from you tube and download it..**")
-    DELAY_BETWEEN_EDITS = 0.1
-    PROCESS_RUN_TIME = 100
+    await event.edit(
+        "**Getting Your Music it may take a few seconds to fetch the song from you tube and download it..**"
+    )
     cmd = event.pattern_match.group(1)
     cmnd = f"{cmd}"
     reply_to_id = event.message.id
@@ -26,8 +20,8 @@ async def _(event):
         reply_to_id = event.reply_to_msg_id
     subprocess.run(["spotdl", "-s", cmnd, "-q", "best"])
     subprocess.run(
-        'for f in *.opus; do      mv -- "$f" "${f%.opus}.mp3"; done',
-        shell=True)
+        'for f in *.opus; do      mv -- "$f" "${f%.opus}.mp3"; done', shell=True
+    )
     l = glob.glob("*.mp3")
     loa = l[0]
     await event.edit("`Yeah, I found the song..🎶`")
@@ -37,7 +31,7 @@ async def _(event):
         force_document=False,
         allow_cache=False,
         supports_streaming=True,
-        caption='Uploaded successfully',
+        caption="Uploaded successfully",
         reply_to=reply_to_id,
     )
     subprocess.run("rm -rf *.mp3", shell=True)
