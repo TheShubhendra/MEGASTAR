c38a1c25757282ab819622cfe5faef9f4f679c5d
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2021-01-01 12:30:55 +0530

    ..

--- var.py
+++ var.py
@@ -2,9 +2,9 @@ import os
 
 ENV = bool(os.environ.get("ENV", False))
 if ENV:
-    from heroku_config import Var as Config
+    from heroku_config import Var as config
 else:
-    from local_config import Development as Config
+    from local_config import Development as config
 
 
-Var = Config
+Var = config
