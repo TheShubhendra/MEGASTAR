03a5c1b2b0999856f9218b0a0cd8f56242aca510
Bristi-OP <missbristimandal@gmail.com>
2021-01-04 18:31:45 +0000

    Pylint Fixes 🚶

--- userbot/plugins/gadmin.py
+++ userbot/plugins/gadmin.py
@@ -1,17 +1,13 @@
-
-from userbot import bot, CMD_HELP
-from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
-from userbot.utils import admin_cmd
-import html
-from telethon import events
-from telethon.tl.functions.photos import GetUserPhotosRequest
-from telethon.tl.functions.users import GetFullUserRequest
-from telethon.tl.types import MessageEntityMentionName
-from telethon.utils import get_input_location
 from telethon.events import ChatAction
+from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
+from telethon.tl.types import MessageEntityMentionName
 
-async def get_full_user(event):  
-    args = event.pattern_match.group(1).split(':', 1)
+from userbot import CMD_HELP
+from userbot.utils import admin_cmd
+
+
+async def get_full_user(event):
+    args = event.pattern_match.group(1).split(":", 1)
     extra = None
     if event.reply_to_msg_id and not len(args) == 2:
         previous_message = await event.get_reply_message()
@@ -28,15 +24,16 @@ async def get_full_user(event):
             return
         if event.message.entities is not None:
             probable_user_mention_entity = event.message.entities[0]
-            if isinstance(probable_user_mention_entity,
-                          MessageEntityMentionName):
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
                 user_id = probable_user_mention_entity.user_id
                 user_obj = await event.client.get_entity(user_id)
                 return user_obj
         try:
             user_obj = await event.client.get_entity(user)
         except Exception as err:
-            return await event.edit("Error... Please report at @MEGASTAR_USERBOT", str(err))           
+            return await event.edit(
+                "Error... Please report at @MEGASTAR_USERBOT", str(err)
+            )
     return user_obj, extra
 
 
@@ -50,15 +47,16 @@ async def get_user_from_id(user, event):
         return None
     return user_obj
 
+
 @borg.on(admin_cmd(pattern="gban ?(.*)"))
 async def gban(userbot):
     dc = userbot
     sender = await dc.get_sender()
     me = await dc.client.get_me()
     if not sender.id == me.id:
-        dark = await dc.reply("Gbanning This User !")
+        await dc.reply("Gbanning This User !")
     else:
-        dark = await dc.edit("Wait Processing.....")
+        await dc.edit("Wait Processing.....")
     me = await userbot.client.get_me()
     await mega.edit(f"Preparing to ban you globally😈😈")
     my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
@@ -72,25 +70,23 @@ async def gban(userbot):
         userbot.chat.title
     try:
         user, reason = await get_full_user(userbot)
-    except:
+    except BaseException:
         pass
     try:
         if not reason:
             reason = "Private"
-    except:
+    except BaseException:
         return await mega.edit(f"**Something went Wrong 🤔**")
     if user:
         if user.id == 1317466348 or user.id == 1356768472:
-            return await mega.edit(
-                f"**WHY WOULD I BAN MY DEV?? ARE YOU CRAZY?**"
-            )
+            return await mega.edit(f"**WHY WOULD I BAN MY DEV?? ARE YOU CRAZY?**")
         try:
             from userbot.modules.sql_helper.gmute_sql import gmute
-        except:
+        except BaseException:
             pass
         try:
             await userbot.client(BlockRequest(user))
-        except:
+        except BaseException:
             pass
         testuserbot = [
             d.entity.id
@@ -102,14 +98,14 @@ async def gban(userbot):
                 await userbot.client.edit_permissions(i, user, view_messages=False)
                 a += 1
                 await mega.edit(f"**Globally banning.. Total Affected Chats **: `{a}`")
-            except:
+            except BaseException:
                 b += 1
     else:
         await mega.edit(f"**Reply to a user sur !!**")
     try:
         if gmute(user.id) is False:
             return await mega.edit(f"**Error! User already gbanned.**")
-    except:
+    except BaseException:
         pass
     return await mega.edit(
         f"**Globally banned this fool😈 [{user.first_name}](tg://user?id={user.id}) Affected Chats😏😉 : {a} **"
@@ -122,9 +118,9 @@ async def gunben(userbot):
     sender = await dc.get_sender()
     me = await dc.client.get_me()
     if not sender.id == me.id:
-        dark = await dc.reply("`Yaa.. lemme ungban this crazy again`")
+        await dc.reply("`Yaa.. lemme ungban this crazy again`")
     else:
-        dark = await dc.edit("Weit n watch ! ")
+        await dc.edit("Weit n watch ! ")
     me = await userbot.client.get_me()
     await mega.edit(f"Trying To Ungban User !")
     my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
@@ -138,23 +134,25 @@ async def gunben(userbot):
         userbot.chat.title
     try:
         user, reason = await get_full_user(userbot)
-    except:
+    except BaseException:
         pass
     try:
         if not reason:
             reason = "Private"
-    except:
+    except BaseException:
         return await mega.edit("Someting Went Wrong 🤔")
     if user:
         if user.id == 1317466348 or user.id == 1356768472:
-            return await mega.edit("**You crazzzyyy..you can't gban or ungban my creator... !**")
+            return await mega.edit(
+                "**You crazzzyyy..you can't gban or ungban my creator... !**"
+            )
         try:
             from userbot.modules.sql_helper.gmute_sql import ungmute
-        except:
+        except BaseException:
             pass
         try:
             await userbot.client(UnblockRequest(user))
-        except:
+        except BaseException:
             pass
         testuserbot = [
             d.entity.id
@@ -165,48 +163,56 @@ async def gunben(userbot):
             try:
                 await userbot.client.edit_permissions(i, user, send_messages=True)
                 a += 1
-                await mega.edit(f"**Ungbaning this crazy guy 🙄😁... AFFECTED CHATS - {a} **")
-            except:
+                await mega.edit(
+                    f"**Ungbaning this crazy guy 🙄😁... AFFECTED CHATS - {a} **"
+                )
+            except BaseException:
                 b += 1
     else:
         await mega.edit("**Reply to a user you crazy**")
     try:
         if ungmute(user.id) is False:
             return await mega.edit("**Error! User already ungbanned.**")
-    except:
+    except BaseException:
         pass
     return await mega.edit(
         f"**Ungbanned this crazy guy. gibbing just a last chance... ; USER - [{user.first_name}](tg://user?id={user.id}) CHATS : {a} **"
     )
 
 
-
-
 @borg.on(ChatAction)
-async def handler(rkG): 
-   if rkG.user_joined or rkG.user_added:      
-       try:       	
-         from userbot.modules.sql_helper.gmute_sql import is_gmuted
-         guser = await rkG.get_user()      
-         gmuted = is_gmuted(guser.id)             
-       except:      
-          return
-       if gmuted:
-        for i in gmuted:
-            if i.sender == str(guser.id):                                                                         
-                chat = await rkG.get_chat()
-                admin = chat.admin_rights
-                creator = chat.creator   
-                if admin or creator:
-                 try:
-                    await client.edit_permissions(rkG.chat_id, guser.id, view_messages=False)                              
-                    await rkG.reply(
-                     f"**Gbanned User(the ultimate nub nibba) Joined the chat!!** \n"                      
-                     f"**Victim Id**: [{guser.id}](tg://user?id={guser.id})\n"                   
-                     f"**Action **  : `Banned this crazy guy.. again...Sed..`")                                                
-                 except:       
-                    rkG.reply("`No Permission To Ban.. @admins please ban him he is a globally banned user and a potential spammer...!`")                   
-                    return 
+async def handler(rkG):
+    if rkG.user_joined or rkG.user_added:
+        try:
+            from userbot.modules.sql_helper.gmute_sql import is_gmuted
+
+            guser = await rkG.get_user()
+            gmuted = is_gmuted(guser.id)
+        except BaseException:
+            return
+        if gmuted:
+            for i in gmuted:
+                if i.sender == str(guser.id):
+                    chat = await rkG.get_chat()
+                    admin = chat.admin_rights
+                    creator = chat.creator
+                    if admin or creator:
+                        try:
+                            await client.edit_permissions(
+                                rkG.chat_id, guser.id, view_messages=False
+                            )
+                            await rkG.reply(
+                                f"**Gbanned User(the ultimate nub nibba) Joined the chat!!** \n"
+                                f"**Victim Id**: [{guser.id}](tg://user?id={guser.id})\n"
+                                f"**Action **  : `Banned this crazy guy.. again...Sed..`"
+                            )
+                        except BaseException:
+                            rkG.reply(
+                                "`No Permission To Ban.. @admins please ban him he is a globally banned user and a potential spammer...!`"
+                            )
+                            return
+
+
 CMD_HELP.update(
     {
         "gadmin": "**Plugin : **`gadmin`\
