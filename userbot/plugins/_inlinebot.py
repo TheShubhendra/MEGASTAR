0d32b72cdfa88487913bf85a6b0b9e056bb834fd
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2021-01-04 09:29:48 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -165,7 +165,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Please get your own LEGEND userbot, and don't use mine! Join @LEGEND_USERBOT_SUPPORT for help"
+            reply_pop_up_alert = "Please get your own Megastar userbot, and don't use mine! Join @MEGASTAR_SUPPORT for help"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(
@@ -182,7 +182,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Please get your own LEGEND userbot, and don't use mine! Join @LEGEND_USERBOT_SUPPORT for help "
+            reply_pop_up_alert = "Please get your own Megastar userbot, and don't use mine! Join @MEGASTAR_SUPPORT for help "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.*)")))
