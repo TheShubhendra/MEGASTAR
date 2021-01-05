9bb944700607c18a8e2b4de1fb8b1da62fb968e6
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2021-01-05 22:51:14 +0530

    Update images.py

--- userbot/plugins/images.py
+++ userbot/plugins/images.py
@@ -1,60 +1,52 @@
-"""
-Download & Upload Images on Telegram\n
+"""Download & Upload Images on Telegram\n
 Syntax: `.img <Name>` or `.img (replied message)`
-\n Upgraded and Google Image Error Fixed by @NeoMatrix90 aka @kirito6969
-from oub
+\n Upgraded and Google Image Error Fixed
 """
+
+from userbot.google_img import googleimagesdownload
 import os
 import shutil
+from re import findall
+from userbot.utils import admin_cmd
 
-from .. import CMD_HELP
-from ..utils import admin_cmd, edit_or_reply
 
-
-@borg.on(admin_cmd(pattern=r"img(?: |$)(\d*)? ?(.*)"))
+@borg.on(admin_cmd(pattern="img ?(.*)"))
 async def img_sampler(event):
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    if event.is_reply and not event.pattern_match.group(2):
-        query = await event.get_reply_message()
-        query = str(query.message)
+    await event.edit("`Lemme find your image😁....`")
+    reply = await event.get_reply_message()
+    if event.pattern_match.group(1):
+        query = event.pattern_match.group(1)
+    elif reply:
+        query = reply.message
     else:
-        query = str(event.pattern_match.group(2))
-    if not query:
-        return await edit_or_reply(
-            event, "Reply to a message or pass a query to search!"
-        )
-    pagal = await edit_or_reply(event, "`Processing...`")
-    if event.pattern_match.group(1) != "":
-        lim = int(event.pattern_match.group(1))
-        if lim > 10:
-            lim = int(10)
-        if lim <= 0:
-            lim = int(1)
-    else:
-        lim = int(3)
+    	await event.edit("`um, mind mentioning what I actually need to search for ;_;`")
+    	return
+        
+    lim = findall(r"lim=\d+", query)
+    # lim = event.pattern_match.group(1)
+    try:
+        lim = lim[0]
+        lim = lim.replace("lim=", "")
+        query = query.replace("lim=" + lim[0], "")
+    except IndexError:
+        lim = 5
     response = googleimagesdownload()
+
     # creating list of arguments
     arguments = {
         "keywords": query,
         "limit": lim,
         "format": "jpg",
-        "no_directory": "no_directory",
+        "no_directory": "no_directory"
     }
+
     # passing the arguments to the function
-    try:
-        paths = response.download(arguments)
-    except Exception as e:
-        return await pagal.edit(f"Error: \n`{e}`")
+    paths = response.download(arguments)
     lst = paths[0][query]
-    await bot.send_file(
-        await bot.get_input_entity(event.chat_id), lst, reply_to=reply_to_id
-    )
+    await event.client.send_file(await event.client.get_input_entity(event.chat_id), lst)
     shutil.rmtree(os.path.dirname(os.path.abspath(lst[0])))
-    await pagal.delete()
-
-
+    await event.delete()
+ 
 CMD_HELP.update(
     {
         "images": "**Plugin :**`images`\
