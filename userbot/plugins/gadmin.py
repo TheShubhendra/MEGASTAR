43437feb4bfeba0aaddd507393af5ffc76834f15
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2021-01-05 09:02:36 +0530

    🙄

--- userbot/plugins/gadmin.py
+++ userbot/plugins/gadmin.py
@@ -1,3 +1,4 @@
+from telethon import events
 from telethon.events import ChatAction
 from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
 from telethon.tl.types import MessageEntityMentionName
