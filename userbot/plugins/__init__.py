29d1f026abfb17926eaef752ffc9ec7483b4fb5c
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2021-01-07 14:15:10 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -108,9 +108,8 @@ def check_data_base_heal_th():
         output = "Functioning"
         is_database_working = True
     return is_database_working, output
-async def alive():
+async def megalive():
     _, check_sgnirts = check_data_base_heal_th()
-    sudo = "Enabled" if config.SUDO_USERS else "Disabled"
     uptime = await get_readable_time((time.time() - StartTime))
     try:
         useragent = (
