9d6beafb379ed4b7419e9e87b24ea7998bc5b967
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2021-01-07 09:24:19 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -46,10 +46,10 @@ async def cmd_list(event):
                 await event.edit(input_str + " is not a valid plugin!")
         else:
             help_string = f"**Userbot Helper.. Provided by ✨{DEFAULTUSER}✨ \n`Userbot Helper to reveal all the commands** 🥳`\n__Do .help plugin_name for commands, in case popup doesn't appear.__ @MEGASTAR_SUPPORT"
-            result = await bot.inline_query(  # pylint:disable=E0602
+            results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, "Userbot"
             )
-            await result[0].click(
+            await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
             await event.delete()
