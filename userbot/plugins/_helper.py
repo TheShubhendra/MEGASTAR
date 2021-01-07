5b30212ada05512f40dd27cf2509188dac7b71f2
Bristi-OP <missbristimandal@gmail.com>
2021-01-07 07:28:08 +0000

    Pylint Fixes 🚶

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -50,7 +50,7 @@ async def cmd_list(event):
         else:
             help_string = f"Userbot Helper.. Provided by ✨{DEFAULTUSER}✨ \n`Userbot Helper to reveal all the commands`\n__Do .help plugin_name for commands, in case popup doesn't appear.__"
             results = await bot.inline_query(  # pylint:disable=E0602
-                tgbotusername, 'Userbot'
+                tgbotusername, "Userbot"
             )
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
