902d109bd64e7c1ca2460811b63ea23e258da175
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2021-01-07 22:05:30 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -110,7 +110,7 @@ def check_data_base_heal_th():
     return is_database_working, output
 
 
-async def bristialive():
+async def alive():
     _, check_sgnirts = check_data_base_heal_th()
     sudo = "Enabled" if Config.SUDO_USERS else "Disabled"
     uptime = await get_readable_time((time.time() - StartTime))
