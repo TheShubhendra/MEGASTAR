45036f682ce84ae501c019cf6d279d2d145d1e52
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2021-01-07 14:47:45 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -108,12 +108,51 @@ def check_data_base_heal_th():
         output = "Functioning"
         is_database_working = True
     return is_database_working, output
-async def megalive():
+async def catalive():
     _, check_sgnirts = check_data_base_heal_th()
+    sudo = "Enabled" if Config.SUDO_USERS else "Disabled"
     uptime = await get_readable_time((time.time() - StartTime))
     try:
         useragent = (
             "Mozilla/5.0 (Linux; Android 10; SM-G975F) "
             "AppleWebKit/537.36 (KHTML, like Gecko) "
             "Chrome/80.0.3987.149 Mobile Safari/537.36"
-                    )
+        )
+        user_id = Heroku.account().id
+        headers = {
+            "User-Agent": useragent,
+            "Authorization": f"Bearer {Config.HEROKU_API_KEY}",
+            "Accept": "application/vnd.heroku+json; version=3.account-quotas",
+        }
+        path = "/accounts/" + user_id + "/actions/get-quota"
+        r = requests.get(heroku_api + path, headers=headers)
+        result = r.json()
+        quota = result["account_quota"]
+        quota_used = result["quota_used"]
+
+        # Used
+        remaining_quota = quota - quota_used
+        math.floor(remaining_quota / quota * 100)
+        minutes_remaining = remaining_quota / 60
+        hours = math.floor(minutes_remaining / 60)
+        minutes = math.floor(minutes_remaining % 60)
+        # Current
+        App = result["apps"]
+        try:
+            App[0]["quota_used"]
+        except IndexError:
+            AppQuotaUsed = 0
+        else:
+            AppQuotaUsed = App[0]["quota_used"] / 60
+            math.floor(App[0]["quota_used"] * 100 / quota)
+        AppHours = math.floor(AppQuotaUsed / 60)
+        AppMinutes = math.floor(AppQuotaUsed % 60)
+        dyno = f"{AppHours}h {AppMinutes}m/{hours}h {minutes}m"
+    except Exception as e:
+        dyno = e
+    return f"Catuserbot Stats\
+                 \n\nDatabase : {check_sgnirts}\
+                  \nSudo : {sudo}\
+                  \nUptime : {uptime}\
+                  \nDyno : {dyno}\
+                  "
