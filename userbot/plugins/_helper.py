df3744bc1acf1541c2caa12476fd87e168caa941
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2021-01-07 22:06:31 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -2,7 +2,7 @@ import asyncio
 
 from telethon import functions
 
-from . import ALIVE_NAME, CMD_HELP
+from . import ALIVE_NAME, CMD_HELP, alive
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Megastar"
 
