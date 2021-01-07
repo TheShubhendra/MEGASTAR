61cf41dd5b38cc6a9ca949d33b79de41d290d2ac
Bristi-OP <missbristimandal@gmail.com>
2021-01-07 10:17:54 +0000

    Pylint Fixes 🚶

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -54,7 +54,9 @@ if config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
                 "© Megastart",
-                text="{}\n**Currently Loaded Plugins**: {}".format(query, len(CMD_LIST)),
+                text="{}\n**Currently Loaded Plugins**: {}".format(
+                    query, len(CMD_LIST)
+                ),
                 buttons=buttons,
                 link_preview=False,
             )
