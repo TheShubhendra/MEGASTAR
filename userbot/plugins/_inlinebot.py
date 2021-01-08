04f619611f2e2a913426c80410b4242de223e94b
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2021-01-08 16:19:19 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -53,7 +53,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     buttons=buttons,
                 )
             await event.answer([result] if result else None)
-        elif event.query.user_id == bot.uid and query.startswith("**Userbot**"):
+        elif event.query.user_id == bot.uid and query.startswith("Userbot"):
             rev_text = query[::-1]
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
