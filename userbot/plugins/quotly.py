7b1e6a541002d654df22a6e85f467216a0fa2a9a
Bristi-OP <missbristimandal@gmail.com>
2021-01-09 07:54:48 +0000

    Pylint Fixes 🚶

--- userbot/plugins/quotly.py
+++ userbot/plugins/quotly.py
@@ -3,11 +3,10 @@ imported from nicegrill
 modified by @mrconfused
 QuotLy: Avaible commands: .qbot
 """
-import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from userbot import bot, CMD_HELP
+
+from userbot import CMD_HELP, bot
 from userbot.utils import admin_cmd
 
 
@@ -39,33 +38,39 @@ async def stickerchat(quotes):
     os.remove("./temp/sticker.webp")
 
 
-
 @borg.on(admin_cmd(pattern=r"qbot"))
 async def _(event):
     if event.fwd_from:
-        return 
+        return
     if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user message.```")
-       return
-    reply_message = await event.get_reply_message() 
-    
+        await event.edit("```Reply to any user message.```")
+        return
+    reply_message = await event.get_reply_message()
+
     chat = "@QuotLyBot"
-    sender = reply_message.sender
-    
+    reply_message.sender
+
     await event.edit("Making a Quote")
     async with bot.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
-              await bot.forward_messages(chat, reply_message)
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("Please unblock @QuotLyBot and try again")
-              return
-          if response.text.startswith("Hi!"):
-             await event.edit("Can you kindly disable your forward privacy settings for good?")
-          else: 
-             await event.delete()   
-             await event.client.send_file(event.chat_id, response.message , reply_to=reply_message) 
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=1031952739)
+            )
+            await bot.forward_messages(chat, reply_message)
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("Please unblock @QuotLyBot and try again")
+            return
+        if response.text.startswith("Hi!"):
+            await event.edit(
+                "Can you kindly disable your forward privacy settings for good?"
+            )
+        else:
+            await event.delete()
+            await event.client.send_file(
+                event.chat_id, response.message, reply_to=reply_message
+            )
+
 
 CMD_HELP.update(
     {
