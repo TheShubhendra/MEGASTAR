2607503ada82c5cf0bf03a1f3d26c91d8a26d20d
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2021-01-10 16:56:33 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -104,7 +104,7 @@ if Var.PRIVATE_GROUP_ID is not None:
                 if pmpermit_sql.is_approved(chat.id):
                     pmpermit_sql.disapprove(chat.id)
                     await event.edit(
-                        "Disapproved [{}](tg://user?id={})".format(firstname, chat.id)
+                        "Disapproved [{firstname}](tg://user?id={})".format(firstname, chat.id)
                     )
                     await asyncio.sleep(3)
                     await event.delete()
