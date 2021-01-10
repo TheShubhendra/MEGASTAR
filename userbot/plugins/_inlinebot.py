469d66ec9874bd6d0555b2f4a4dd8d844d947800
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2021-01-10 08:16:00 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -245,7 +245,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
     async def on_plug_in_callback_query_handler(event):
         if event.query.user_id == bot.uid:
-            await event.edit("Help .menu closed")
+            await event.edit("Help menu closed ^_^")
         else:
             reply_pop_up_alert = "Please get your own MEGASTAR userbot, and don't use mine! Join @MEGASTAR_SUPPORT for help "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
