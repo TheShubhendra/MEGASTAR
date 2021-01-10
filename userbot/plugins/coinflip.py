a476fd14be1ae1377380ffc57bceb3830cebfb34
Bristi-OP <missbristimandal@gmail.com>
2021-01-10 03:54:01 +0000

    Pylint Fixes 🚶

--- userbot/plugins/coinflip.py
+++ userbot/plugins/coinflip.py
@@ -1,8 +1,8 @@
 """CoinFlip for @UniBorg
 Syntax: .coinflip [optional_choice]"""
 # useless plugin
-from telethon import events
-import random, re
+import random
+
 from userbot.utils import admin_cmd
 
 
@@ -18,15 +18,19 @@ async def _(event):
         if input_str == "heads":
             await event.edit("The coin landed on: **Heads**. \n You were correct.")
         elif input_str == "tails":
-            await event.edit("The coin landed on: **Heads**. \n You weren't correct, try again ...")
+            await event.edit(
+                "The coin landed on: **Heads**. \n You weren't correct, try again ..."
+            )
         else:
             await event.edit("The coin landed on: **Heads**.")
     elif r % 2 == 0:
         if input_str == "tails":
             await event.edit("The coin landed on: **Tails**. \n You were correct.")
         elif input_str == "heads":
-            await event.edit("The coin landed on: **Tails**. \n You weren't correct, try again ...")
+            await event.edit(
+                "The coin landed on: **Tails**. \n You weren't correct, try again ..."
+            )
         else:
             await event.edit("The coin landed on: **Tails**.")
     else:
-        await event.edit("¯\_(ツ)_/¯")
+        await event.edit(r"¯\_(ツ)_/¯")
