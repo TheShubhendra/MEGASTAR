8389f3b025389ee7706dbafe3e2ba908a6962fae
Bristi-OP <missbristimandal@gmail.com>
2021-01-11 17:25:04 +0000

    Pylint Fixes 🚶

--- userbot/plugins/gita.py
+++ userbot/plugins/gita.py
@@ -1,7 +1,7 @@
 # Author: Shubhendra Kushswaha (@TheShubhendra)
 # Email: shubhendrakushwaha94@gmail.com
-import os
 import pygita
+
 from userbot.utils import admin_cmd
 
 CLIENT_ID = Var.GITA_CLIENT_ID
@@ -16,21 +16,29 @@ if CLIENT_ID and CLIENT_SECRET:
 async def gita(event):
     """ To get a specific verse from a specific chapter in English. """
     if CLIENT_ID is None or CLIENT_SECRET is None:
-        await edit_delete(event, "`Please add required GITA_CLIENT_SECRET and GITA_CLIENT_ID env var`", 10)
+        await edit_delete(
+            event,
+            "`Please add required GITA_CLIENT_SECRET and GITA_CLIENT_ID env var`",
+            10,
+        )
         return
     chapter_number = int(event.pattern_match.group(1))
     verse_number = int(event.pattern_match.group(2))
     verse = pygita.get_verse(chapter_number, verse_number, language="en")
-    await event.edit(f'**{verse.text}** {verse.meaning}')
+    await event.edit(f"**{verse.text}** {verse.meaning}")
 
 
 @borg.on(admin_cmd(pattern="gita +(.*) +(.*) hi"))
 async def gita(event):
     """ To get a specific verse from a specific chapter in Hindi. """
     if CLIENT_ID is None or CLIENT_SECRET is None:
-        await edit_delete(event, "`Please add required GITA_CLIENT_SECRET and GITA_CLIENT_ID env var`", 10)
+        await edit_delete(
+            event,
+            "`Please add required GITA_CLIENT_SECRET and GITA_CLIENT_ID env var`",
+            10,
+        )
         return
     chapter_number = int(event.pattern_match.group(1))
     verse_number = int(event.pattern_match.group(2))
     verse = pygita.get_verse(chapter_number, verse_number, language="hi")
-    await event.edit(f'**{verse.text}** {verse.meaning}')
+    await event.edit(f"**{verse.text}** {verse.meaning}")
