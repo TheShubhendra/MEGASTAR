ebc2be78da3ab843811513bdd3ee988c64b3d05d
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-01-11 22:06:46 +0530

    Fix enivorment variable error😅😅

--- userbot/plugins/gita.py
+++ userbot/plugins/gita.py
@@ -4,18 +4,18 @@ import os
 import pygita
 from userbot.utils import admin_cmd
 
-CLIENT_ID = os.environ.get("GITA_CLIENT_ID")
-CLIENT_SECRET = os.environ.get("GITA_CLIENT_SECRET")
+CLIENT_ID = Var.GITA_CLIENT_ID
+CLIENT_SECRET = Var.GITA_CLIENT_SECRET
 """ Get API crendentials from https://bhagavadgita.io . """
 
-if Var.CLIENT_ID and Var.CLIENT_SECRET:
+if CLIENT_ID and CLIENT_SECRET:
     pygita.auth(CLIENT_ID, CLIENT_SECRET)
 
 
 @borg.on(admin_cmd(pattern="gita +(.*) +(.*)"))
 async def gita(event):
     """ To get a specific verse from a specific chapter in English. """
-    if Var.CLIENT_ID is None or Var.CLIENT_SECRET is None:
+    if CLIENT_ID is None or CLIENT_SECRET is None:
         await edit_delete(event, "`Please add required GITA_CLIENT_SECRET and GITA_CLIENT_ID env var`", 10)
         return
     chapter_number = int(event.pattern_match.group(1))
@@ -27,7 +27,7 @@ async def gita(event):
 @borg.on(admin_cmd(pattern=""))
 async def gita(event):
     """ To get a specific verse from a specific chapter in Hindi. """
-    if Var.CLIENT_ID is None or Var.CLIENT_SECRET is None:
+    if CLIENT_ID is None or CLIENT_SECRET is None:
         await edit_delete(event, "`Please add required GITA_CLIENT_SECRET and GITA_CLIENT_ID env var`", 10)
         return
     chapter_number = int(event.pattern_match.group(1))
