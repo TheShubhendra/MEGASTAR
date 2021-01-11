9c699bfc965bada212acd200e69178de60b1483d
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2021-01-11 23:06:19 +0530

    Update invite.py

--- userbot/plugins/invite.py
+++ userbot/plugins/invite.py
@@ -1,9 +1,8 @@
 """Invite the user(s) to the current chat
 Syntax: .invite <User(s)>"""
-
+# for MEGASTAR
 from telethon import functions
-
-from ..utils import admin_cmd
+from userbot.utils import admin_cmd
 
 
 @borg.on(admin_cmd(pattern="invite ?(.*)"))
@@ -12,31 +11,29 @@ async def _(event):
         return
     to_add_users = event.pattern_match.group(1)
     if event.is_private:
-        await edit_delete(
-            event, "`.invite` users to a chat, not to a Private Message", 5
-        )
+        await event.edit("`.invite` users to a chat, not to a Private Message")
     else:
+        logger.info(to_add_users)
         if not event.is_channel and event.is_group:
             # https://lonamiwebs.github.io/Telethon/methods/messages/add_chat_user.html
             for user_id in to_add_users.split(" "):
                 try:
-                    await event.client(
-                        functions.messages.AddChatUserRequest(
-                            chat_id=event.chat_id, user_id=user_id, fwd_limit=1000000
-                        )
-                    )
+                    await borg(functions.messages.AddChatUserRequest(
+                        chat_id=event.chat_id,
+                        user_id=user_id,
+                        fwd_limit=1000000
+                    ))
                 except Exception as e:
-                    await edit_delete(event, f"`{str(e)}`", 5)
+                    await event.reply(str(e))
+            await event.edit("Invited Successfully")
         else:
             # https://lonamiwebs.github.io/Telethon/methods/channels/invite_to_channel.html
             for user_id in to_add_users.split(" "):
                 try:
-                    await event.client(
-                        functions.channels.InviteToChannelRequest(
-                            channel=event.chat_id, users=[user_id]
-                        )
-                    )
+                    await borg(functions.channels.InviteToChannelRequest(
+                        channel=event.chat_id,
+                        users=[user_id]
+                    ))
                 except Exception as e:
-                    await edit_delete(event, f"`{str(e)}`", 5)
-
-        await edit_or_reply(event, f"`{to_add_users} is/are Invited Successfully`")
+                    await event.reply(str(e))
+            await event.edit("Invited Successfully..!!!")
