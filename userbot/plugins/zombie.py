8e15f5aa53b94b4ca39ecfe8b061474dbcce62c2
Bristi-OP <missbristimandal@gmail.com>
2021-01-15 03:28:45 +0000

    Pylint Fixes 🚶

--- userbot/plugins/zombie.py
+++ userbot/plugins/zombie.py
@@ -1,21 +1,13 @@
 # salute the creator
-from telethon import events
-from userbot.utils import admin_cmd
 from asyncio import sleep
-from os import remove
-from userbot import CMD_HELP
-from telethon.errors import (BadRequestError, ChatAdminRequiredError,
-                             ImageProcessFailedError, PhotoCropSizeSmallError,
-                             UserAdminInvalidError)
-from telethon.errors.rpcerrorlist import (UserIdInvalidError,
-                                          MessageTooLongError)
-from telethon.tl.functions.channels import (EditAdminRequest,
-                                            EditBannedRequest,
-                                            EditPhotoRequest)
-from telethon.tl.types import (ChannelParticipantsAdmins, ChatAdminRights,
-                               ChatBannedRights, MessageEntityMentionName,
-                               MessageMediaPhoto)
 
+from telethon import events
+from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
+from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.types import ChatBannedRights
+
+from userbot import CMD_HELP
+from userbot.utils import admin_cmd
 
 # ============== CONSTANT================ #
 
@@ -46,7 +38,6 @@ MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=True)
 
 UNMUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
 # ================================================
-
 
 
 @borg.on(admin_cmd(pattern=f"zombie", allow_sudo=True))
@@ -89,17 +80,16 @@ async def rm_deletedacc(show):
         if user.deleted:
             try:
                 await show.client(
-                    EditBannedRequest(show.chat_id, user.id, BANNED_RIGHTS))
+                    EditBannedRequest(show.chat_id, user.id, BANNED_RIGHTS)
+                )
             except ChatAdminRequiredError:
                 await show.edit("I don't have ban rights in this group")
                 return
             except UserAdminInvalidError:
                 del_u -= 1
                 del_a += 1
-            await show.client(
-                EditBannedRequest(show.chat_id, user.id, UNBAN_RIGHTS))
+            await show.client(EditBannedRequest(show.chat_id, user.id, UNBAN_RIGHTS))
             del_u += 1
-
 
     if del_u > 0:
         del_status = f"Cleaned **{del_u}** deleted account(s)"
@@ -108,17 +98,18 @@ async def rm_deletedacc(show):
         del_status = f"Cleaned **{del_u}** deleted account(s) \
         \n**{del_a}** deleted admin accounts are not removed"
 
-
     await show.edit(del_status)
     await sleep(2)
     await show.delete()
 
-
     if config.PM_LOGGR_BOT_API_ID is not None:
         await show.client.send_message(
-            config.PM_LOGGR_BOT_API_ID, "#CLEANUP\n"
+            config.PM_LOGGR_BOT_API_ID,
+            "#CLEANUP\n"
             f"Cleaned **{del_u}** deleted account(s) !!\
-            \nCHAT: {show.chat.title}({show.chat_id})")
+            \nCHAT: {show.chat.title}({show.chat_id})",
+        )
+
 
 CMD_HELP.update(
     {
