af5176742409f1c7de1cf57d34d61611368cca73
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-01-16 09:02:21 +0530

    Fix absolute import

--- userbot/plugins/quotly.py
+++ userbot/plugins/quotly.py
@@ -8,7 +8,7 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 from userbot import CMD_HELP, bot
 from userbot.utils import admin_cmd
-import userbot.helpers.process
+from userbot.helpers import process
 
 @borg.on(admin_cmd(pattern="q(?: |$)(.*)", outgoing=True))
 async def stickerchat(quotes):
