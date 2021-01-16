b92462653cd2a10dd18c69f39a5fd199da029720
Bristi-OP <missbristimandal@gmail.com>
2021-01-16 14:26:29 +0000

    Pylint Fixes 🚶

--- userbot/plugins/toonify.py
+++ userbot/plugins/toonify.py
@@ -1,27 +1,25 @@
 # salute the creator/creators
 
-from telethon.tl.types import MessageMediaPhoto
-import os, urllib, requests, asyncio
-from userbot.utils import admin_cmd
-from userbot import CMD_HELP
+import os
 
+import requests
+
+from userbot import CMD_HELP
+from userbot.utils import admin_cmd
 
 Mega = config.DEEP_AI if config.DEEP_AI else "quickstart-QUdJIGlzIGNvbWluZy4uLi4K"
+
+
 @borg.on(admin_cmd(pattern="toonify", outgoing=True))
 async def _(event):
-    
-               
+
     reply = await event.get_reply_message()
     if not reply:
-        return await event.edit(
-           "Reply to any image or non animated sticker !"
-        )
+        return await event.edit("Reply to any image or non animated sticker !")
     devent = await event.edit("... Lemme download the file 😉")
     media = await event.client.download_media(reply)
     if not media.endswith(("png", "jpg", "webp")):
-        return await event.edit(
-             "Reply to any image or non animated sticker !"
-        )
+        return await event.edit("Reply to any image or non animated sticker !")
     devent = await event.edit("Toonifying image 😝...")
     r = requests.post(
         "https://api.deepai.org/api/toonify",
@@ -32,22 +30,20 @@ async def _(event):
     )
     os.remove(media)
     if "status" in r.json():
-        return await devent.edit( r.json()["status"])
+        return await devent.edit(r.json()["status"])
     r_json = r.json()["output_url"]
     pic_id = r.json()["id"]
-    
+
     link = f"https://api.deepai.org/job-view-file/{pic_id}/inputs/image.jpg"
     result = f"{r_json}"
-    
+
     await devent.delete()
-    await borg.send_message(
-        event.chat_id,
-        file=result
-    )
+    await borg.send_message(event.chat_id, file=result)
+
 
 CMD_HELP.update(
     {
-        "toonify": 
-    ".toonify <reply to any media where a good face is there> "
-    "\nIt Toonifies the face 🤣😂   (Note :-if its not working then go to deepai.org then get api and set var DEEP_AI nd key.)"
-    })
+        "toonify": ".toonify <reply to any media where a good face is there> "
+        "\nIt Toonifies the face 🤣😂   (Note :-if its not working then go to deepai.org then get api and set var DEEP_AI nd key.)"
+    }
+)
