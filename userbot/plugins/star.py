518e3ba927bed24cc5bf5d2a4587fafe65f21f69
Bristi-OP <missbristimandal@gmail.com>
2021-01-17 16:58:10 +0000

    Pylint Fixes 🚶

--- userbot/plugins/star.py
+++ userbot/plugins/star.py
@@ -1,12 +1,10 @@
-import asyncio
-import os
 from datetime import datetime
-from pathlib import Path
 
 from userbot import ALIVE_NAME, bot
 from userbot.utils import admin_cmd
-from userbot.utils import edit_or_reply as eor
+
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "megastar"
+
 
 @borg.on(admin_cmd(pattern=r"star", outgoing=True))
 async def _(event):
@@ -15,5 +13,7 @@ async def _(event):
     hmm = bot.uid
     start = datetime.now()
     end = datetime.now()
-    ms = (end - start).microseconds /1000
-    await event.edit(f"༒➩ֆтαʀ ιֆ ɦɛʀɛ 😉 \n ➥ мʏ βσֆֆ ☞ [{DEFAULTUSER}](tg://user?id={hmm})\n ➥ мʏ ֆρɛɛ∂ ☞ {ms} ms")
+    ms = (end - start).microseconds / 1000
+    await event.edit(
+        f"༒➩ֆтαʀ ιֆ ɦɛʀɛ 😉 \n ➥ мʏ βσֆֆ ☞ [{DEFAULTUSER}](tg://user?id={hmm})\n ➥ мʏ ֆρɛɛ∂ ☞ {ms} ms"
+    )
