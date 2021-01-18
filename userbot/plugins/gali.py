390d149f78c0215a6a03e50f30374cd4d1d585f6
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2021-01-18 16:10:27 +0530

    Update gali.py

--- userbot/plugins/gali.py
+++ userbot/plugins/gali.py
@@ -1,95 +1,22 @@
+"""COMMAND : .abuse"""
 import asyncio
-import random
-
-from ..utils import admin_cmd, edit_or_reply
-from . import CMD_HELP
-
-
-@borg.on(admin_cmd(outgoing=True, pattern="abuse$"))
-async def abusing(abused):
-    reply_text = random.choice(memes.ABUSE_STRINGS)
-    await edit_or_reply(abused, reply_text)
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd(outgoing=True, pattern="abusehard$"))
-async def fuckedd(abusehard):
-    reply_text = random.choice(memes.ABUSEHARD_STRING)
-    await edit_or_reply(abusehard, reply_text)
-
-
-@borg.on(admin_cmd(outgoing=True, pattern="rendi$"))
-async def metoo(e):
-    txt = random.choice(memes.RENDISTR)
-    await edit_or_reply(e, txt)
-
-
-@borg.on(admin_cmd(outgoing=True, pattern="rape$"))
-async def raping(raped):
-    reply_text = random.choice(memes.RAPE_STRINGS)
-    await edit_or_reply(raped, reply_text)
-
-
-@borg.on(admin_cmd(outgoing=True, pattern="fuck$"))
-async def chutiya(fuks):
-    reply_text = random.choice(memes.CHU_STRINGS)
-    await edit_or_reply(fuks, reply_text)
-
-
-@borg.on(admin_cmd(outgoing=True, pattern="thanos$"))
-async def thanos(thanos):
-    reply_text = random.choice(memes.THANOS_STRINGS)
-    await edit_or_reply(thanos, reply_text)
-
-
-@borg.on(admin_cmd(outgoing=True, pattern="kiss$"))
+@borg.on(admin_cmd("abuse|abusehard"))
 async def _(event):
-    event = await edit_or_reply(event, "`kiss`")
-    animation_interval = 0.2
-    animation_ttl = range(100)
-    animation_chars = ["🤵       👰", "🤵     👰", "🤵  👰", "🤵💋👰"]
-    for i in animation_ttl:
-        await asyncio.sleep(animation_interval)
-        await event.edit(animation_chars[i % 4])
-
-
-@borg.on(admin_cmd(outgoing=True, pattern="fuk$"))
-async def _(event):
-    event = await edit_or_reply(event, "`fuking....`")
-    animation_interval = 0.2
-    animation_ttl = range(100)
-    animation_chars = ["👉       ✊️", "👉     ✊️", "👉  ✊️", "👉✊️💦"]
-    for i in animation_ttl:
-        await asyncio.sleep(animation_interval)
-        await event.edit(animation_chars[i % 4])
-
-
-@borg.on(admin_cmd(outgoing=True, pattern="sex$"))
-async def _(event):
-    event = await edit_or_reply(event, "`sex`")
-    animation_interval = 0.2
-    animation_ttl = range(100)
-    animation_chars = ["🤵       👰", "🤵     👰", "🤵  👰", "🤵👼👰"]
-    for i in animation_ttl:
-        await asyncio.sleep(animation_interval)
-        await event.edit(animation_chars[i % 4])
-
-
-CMD_HELP.update(
-    {
-        "gali": "**plugin : **`gali`\
-        \n\n**Commands :**\
-        \n  •  `.abuse`\
-        \n  •  `.abusehard`\
-        \n  •  `.rendi`\
-        \n  •  `.rape`\
-        \n  •  `.fuck`\
-        \n  •  `.thanos`\
-        \n  •  `.kiss`\
-        \n  •  `.fuk`\
-        \n  •  `.sex`\
-        \n\n**Function :**\
-        \n__First 5 are random gali string generaters__\
-        \n__Last 3 are animations__\
-        "
-    }
-)
+    if event.fwd_from:
+        return
+    mentions = "SUNN MADEERCHOD CHAKKE JAISE SAKAL KE KUTTE, MENE TERI MAA KO ... HATHI KE LUND SE CHODD DALA... TERI MAA KI CHUT KO BHOSADA BANADIYA TERI BHEN KI CHUTT BALLON KI TARAH FOOL GAYI TERA PAREEVAR KO PREGNANT KARDALA, ᴛᴇʀᴇ ᴘᴜʀᴇ ᴋʜᴀɴᴅᴀɴ ᴋɪ ᴍᴀ ᴋᴀ ʙᴀʟᴀᴛᴋᴀʀ ᴋʀᴅᴀʟᴀ ᴛᴇʀɪ ᴍᴀ ᴍᴀʀɢʏɪ ᴛᴇʀɪ ᴍᴀ ᴋɪ ʟᴀsʜ ᴋᴏ ᴄʜᴏᴅ ʀʜᴀ ʜᴜ💋💋💋🤣🤣🤣💦💦💦💦 ʙʜᴇᴇᴍ ᴋɪ sʜᴀᴋᴛɪ 💪 ᴅʜᴏᴏᴍ    ᴍᴀᴄʜᴀʏᴇ 🔥🔥 ᴍᴇʀᴀ ʟᴀɴᴅ  ʜᴀʀsʜ sᴋʏ   ɪsᴋɪ 💦ʙʜᴇɴɴ 💦ᴋɪ🔰 xʜᴜᴛ ᴍᴇ ᴊᴀᴀʏᴇ😂,ᴛᴜᴍᴀʀɪɪ ʙʜᴇɴɴ xʜᴏᴅᴜɴɢᴀᴀ ᴛᴜᴍᴀʀɪɪ ʙʜᴇɴɴ xʜᴏᴅᴜɴɢᴀᴀ ᴏᴀᴅᴅᴅ ᴘᴀʀʀʀʀ ᴄʜᴏᴅᴜᴜᴜᴜᴜ 💦💦💦ᴛᴇʀɪɪɪ ᴍᴀᴀᴀ ᴋᴏ ᴄʜᴏᴅᴜᴜᴜ ʀᴀɴᴅɪɪɪɪ ᴋɪ ᴏʟᴀᴀᴀᴀᴅ ᴛᴇʀɪɪɪɪ ʙʜᴇɴɴɴɴ ᴋɪ ᴄʜᴜᴛᴛᴛᴛᴛ ɢᴀsᴛɪɪɪ ᴋᴇ ʙᴀᴄʜʜʜᴇᴇᴇᴇᴇ 🤔🤔"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
