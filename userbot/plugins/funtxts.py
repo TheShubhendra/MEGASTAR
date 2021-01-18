0d14cd1956b0fd68818bac6bc8effa8c57e89d30
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2021-01-18 15:15:25 +0530

    Update funtxts.py

--- userbot/plugins/funtxts.py
+++ userbot/plugins/funtxts.py
@@ -3,25 +3,25 @@ import nekos
 from ..utils import admin_cmd, edit_or_reply
 
 
-@borg.on(admin_cmd(pattern="tcat$"))
-async def hmm(cat):
-    if cat.fwd_from:
+@borg.on(admin_cmd(pattern="meg"))
+async def hmm(mega):
+    if mega.fwd_from:
         return
     reactcat = nekos.textcat()
-    await edit_or_reply(cat, reactcat)
+    await edit_or_reply(mega, reactcat)
 
 
-@borg.on(admin_cmd(pattern="why$"))
-async def hmm(cat):
-    if cat.fwd_from:
+@borg.on(admin_cmd(pattern="why"))
+async def hmm(mega):
+    if mega.fwd_from:
         return
-    whycat = nekos.why()
-    await edit_or_reply(cat, whycat)
+    whymega = nekos.why()
+    await edit_or_reply(mega, whymega)
 
 
-@borg.on(admin_cmd(pattern="fact$"))
-async def hmm(cat):
-    if cat.fwd_from:
+@borg.on(admin_cmd(pattern="fact"))
+async def hmm(mega):
+    if mega.fwd_from:
         return
-    factcat = nekos.fact()
-    await edit_or_reply(cat, factcat)
+    factmega = nekos.fact()
+    await edit_or_reply(mega, factmega)
