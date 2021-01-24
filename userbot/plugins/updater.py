5d44a54d77abf2adc17be3fa9dc1764d094c6157
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2021-01-25 00:11:50 +0530

    decorate

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -41,7 +41,7 @@ async def gen_chlog(repo, diff):
     d_form = "%d/%m/%y"
     for c in repo.iter_commits(diff):
         ch_log += (
-            f"•[{c.committed_datetime.strftime(d_form)}]: {c.summary} by <{c.author}>\n"
+            f"༒[{c.committed_datetime.strftime(d_form)}]: {c.summary} by ʚ{c.author}ɞ\n"
         )
     return ch_log
 
