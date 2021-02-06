cc381db8dd19f6df45c4fe88673e9e42a499856c
Bristi <72791165+Bristi-OP@users.noreply.github.com>
2021-02-06 16:45:43 +0530

    decorate

--- userbot/plugins/evaluators.py
+++ userbot/plugins/evaluators.py
@@ -9,33 +9,52 @@ from .. import CMD_HELP
 from ..utils import admin_cmd, edit_or_reply
 
 
-@borg.on(admin_cmd(pattern="bash (.*)"))
+from telethon import events
+import subprocess
+from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
+import io
+import asyncio
+import time
+
+
+@command(pattern="^.bash ?(.*)")
 async def _(event):
-    if event.fwd_from or event.via_bot_id:
+    if event.fwd_from:
         return
-    cmd = "".join(event.text.split(maxsplit=1)[1:])
+    DELAY_BETWEEN_EDITS = 0.3
+    PROCESS_RUN_TIME = 100
+    cmd = event.pattern_match.group(1)
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
+    start_time = time.time() + PROCESS_RUN_TIME
     process = await asyncio.create_subprocess_shell(
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
     stdout, stderr = await process.communicate()
-    OUTPUT = f"`{stdout.decode()}`"
-    if len(OUTPUT) > config.MAX_MESSAGE_SIZE_LIMIT:
+    e = stderr.decode()
+    if not e:
+        e = "No Error"
+    o = stdout.decode()
+    if not o:
+        o = "◘ Tip: \nIf you want to see the results of your code, I suggest printing them to stdout."
+    else:
+        _o = o.split("\n")
+        o = "\n".join(_o)
+    OUTPUT = f"◘ QUERY:\n__Command:__\n{cmd} \n◘ PID:\n{process.pid}\n\n◘ stderr: \n{e}\nOutput:\n{o}"
+    if len(OUTPUT) > 4095:
         with io.BytesIO(str.encode(OUTPUT)) as out_file:
-            out_file.name = "bash.text"
-            await event.client.send_file(
+            out_file.name = "exec.text"
+            await bot.send_file(
                 event.chat_id,
                 out_file,
                 force_document=True,
                 allow_cache=False,
                 caption=cmd,
-                reply_to=reply_to_id,
+                reply_to=reply_to_id
             )
             await event.delete()
-    else:
-        await edit_or_reply(event, OUTPUT)
+    await event.edit(OUTPUT)
 
 
 @borg.on(admin_cmd(pattern="exec (.*)"))
