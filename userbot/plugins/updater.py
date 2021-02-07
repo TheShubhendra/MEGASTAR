43f8c63532c1c855f482de4bc08c0340b47be138
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-02-07 14:37:12 +0530

    Update from url if provided

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -12,7 +12,7 @@ from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 from userbot import CMD_HELP
 from userbot.utils import admin_cmd
 
-UPSTREAM_REPO_URL = "https://github.com/Bristi-OP/MEGASTAR"
+UPSTREAM_REPO_URL = environ.get("GITHUB_REPO_URL","https://github.com/Bristi-OP/MEGASTAR")
 HEROKU_API_KEY = Var.HEROKU_API_KEY
 HEROKU_APP_NAME = Var.HEROKU_APP_NAME
 
