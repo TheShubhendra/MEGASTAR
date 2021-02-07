aa2a11f55e4ee890a22690e04103c64afe28b46e
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-02-07 10:44:47 +0530

    Add new plugin : getall

---
+++ userbot/plugins/getall.py
@@ -0,0 +1,15 @@
+# salute to the creator
+from telethon.tl.types import ChannelParticipantsAdmins
+
+from userbot.utils import admin_cmd
+
+
+@borg.on(admin_cmd(pattern=r"getall$", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    text = "`Name | Username | Id`"
+    chat = await event.get_input_chat()
+    async for x in bot.iter_participants(chat, 100):
+        text += f" \n {x.first_name} {x.username} {x.id} "
+    await event.edit(text)
