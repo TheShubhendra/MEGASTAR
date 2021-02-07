5d62a42ad075d7b201e8966be71884def39ad1d6
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-02-07 19:11:57 +0530

    Send Last name too

--- userbot/plugins/getall.py
+++ userbot/plugins/getall.py
@@ -7,9 +7,8 @@ from userbot.utils import admin_cmd
 async def _(event):
     if event.fwd_from:
         return
-    text = "`Name | Username | Id`"
+    text = "`First Name| Last Name | Username | Id`"
     chat = await event.get_input_chat()
-    async for x in bot.iter_participants(chat, 100):
-        text += f"\n**{x.first_name}**  @{x.username}  {x.id} 
-        "
+    async for x in bot.iter_participants(chat, 200):
+        text += f"\n**{x.first_name} | {x.last_name}** | @{x.username} | {x.id}"
     await event.edit(text)
