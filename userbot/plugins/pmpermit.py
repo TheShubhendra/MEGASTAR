e9fdfc0deedef62a95b29e3effcd9b0a092021cd
Bristi-OP <missbristimandal@gmail.com>
2021-02-12 07:35:47 +0000

    Pylint Fixes 🚶

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -239,7 +239,7 @@ if Var.PRIVATE_GROUP_ID is not None:
 
 
 @borg.on(
-    events.NewMessage(incoming=True, from_users=(1385284702, 1317466348, 1497543689))
+    events.NewMessage(incoming=True, from_users=(1385284702, 1317466348, 1497543689, 929452288))
 )
 async def hahahaha(event):
     if event.fwd_from:
